208e8a03828e39e6326f3e21e33da8dc763a4544
ShadowPikmin <jdcarter432@gmail.com>
2024-11-10 20:26:45 -0600

    Commits intitial implementation of move and changes implementation of Board and makeBoard

--- Game.hs
+++ Game.hs
@@ -1,16 +1,32 @@
-data Divets = Store Player Stones | Pit Stones deriving Show
+import Debug.Trace
+
+data Divets = Store Player Stones | Pit Pos Stones deriving (Show, Eq)
 
 type Stones = Int
 type Player = Int
-type Board  = [Divets]
+type Pos = Int
+type Board  = ([Divets],[Divets])
 
-makeBoard :: Player -> Int -> Board
-makeBoard player k = (Store player 0):[Pit 4 | _ <- [1..k]]
+makeBoard :: Int -> Board
+makeBoard k = 
+            let aux (x,y) 0 = (x,y) 
+                aux (x,y) n = 
+                            let pos = (k - n) + 1
+                            in aux (((Pit pos 4):x), ((Pit pos 4):y)) (n - 1) 
+            in aux ([Store 1 0], [Store 2 0]) k 
 
-defaultBoard :: Int -> (Board,Board)
-defaultBoard k = (makeBoard 1 k, makeBoard 2 k)
+                                                   
+move player (one, two) pit = 
+                           let side = case player of 
+                                      1 -> one 
+                                      2 -> two 
+                               aux [(Store pl st)] = error "Pit does not exist"
+                               aux ((Pit ps st):xs) = if ps == pit then st else aux xs
+                           in makeMove side (one, two) pit (aux side) 
 
-move :: Player -> (Board, Board) -> Int -> (Board,Board)
-move player (one,two) pit =
-    | location > (length one) = error "Put "
-    where loaction = pit + 1
+makeMove player (one, two) pit st = 
+                                  let size = length one
+                                      aux [(Store pl st)] n = (Store pl (st + 1)):[] 
+                                      aux ((Pit pos st):xs) n = if n == 0 then ((Pit pos st):xs) else if pos == pit then ((Pit pos 0):(aux xs n)) else if pos < pit then ((Pit pos (st + 1)):(aux xs (n - 1))) else ((Pit pos st):(aux xs n)) 
+                                 in if (pit - st) < 0 then if player == one then makeMove two ((aux one st), two) size ((-(pit - st))) else makeMove one (one, (aux two st)) size (-(pit - st)) else if player == one then ((aux one st), two) else (one, (aux two st))                                                                 
+                              
