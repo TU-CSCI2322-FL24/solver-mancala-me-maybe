bcbe5b41f2a777d3f2799466f1411e2ef1c1ef6b
paul borrego <pborrego@trinity.edu>
2024-11-13 12:08:19 -0600

    data

--- Game.hs
+++ Game.hs
@@ -1,27 +1,34 @@
 import Debug.Trace
+import Data.Maybe
 
 -- story one
 type Position = Int
-data Divet = Store Int | Pit Position Int deriving (Show, Eq)
 data Player = PlayerOne | PlayerTwo deriving (Show, Eq)
-
-type Row = [Divet] -- should be changed to "type Row = ([Pit], Store)" where Divet has been removed
+type Row = ([Int], Int) -- should be changed to "type Row = ([Pit], Store)" where Divet has been removed
 type Board  = (Row, Row)
 
 type Game = (Player, Board)
 
 makeBoard :: Int -> Board
-makeBoard k = 
-    let aux (x, y) 0 = (x, y) 
-        aux (x, y) n = aux (((Pit pos 4):x), ((Pit pos 4):y)) (n - 1)
-            where pos = (k - n) + 1
-    in aux ([Store 0], [Store 0]) k
+makeBoard k =
+    let pits = [0 | _ <- 0..k]
+    in ((pits, 0),(pits, 0))
 
-move :: Player -> Board -> Int -> Board
-move player board pit = 
-    let aux [(Store st)] = error "Pit does not exist"
-        aux ((Pit ps st):xs) = if ps == pit then st else aux xs
-    in makeMove player board pit (aux (sideOf player board))
+move :: Game -> Position -> Board
+move (player, board) pit =
+    let stones = getStones (player,board) pit
+    in if isJust stones then  makeMove player board pit stones
+    else error = "Invalid pit selected"
+
+getStones :: Game -> Position -> Maybe Int
+getStones (player, (one,two)) pit =
+    let side = sideOf player (one,two)
+        val  = safeBangBang (zip side [1..]) pit
+    in if val == 0 then nothing else Just val
+
+safeBangBang :: [(Int,Int)] -> Int -> Int
+safeBangBang [] _ = 0
+safeBangBang ((val,pos):xs) pit = if pos == pit then val else safeBangBang xs pit
 
 sideOf :: Player -> Board -> Row
 sideOf PlayerOne board = fst board
@@ -36,6 +43,16 @@ makeMove PlayerTwo (one, two) pit st
     | pit - st < 0  = makeMove PlayerOne (one, (sowRow two pit st)) size (st - pit)
     | otherwise     = (one, (sowRow two pit st))
     where size = length one
+
+--Useful when we know that all pits will get a piece
+sowAll :: Row -> Row
+sowAll (_,s) = map (\a -> a + 1) s
+
+--returns new row and remaining pieces
+moveCorrectSide :: Row -> Position -> Int -> (Row,Int)
+moveCorrectSide (_ , pits) pos stones =
+    let (before,after) = splitAt pos pits
+        remaining      = stones - after
 
 sowRow :: Row -> Int -> Int -> Row
 sowRow [Store st] pit n = [Store (st + 1)]
