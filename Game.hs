6a0612d2a49cf7fbf8347dd85e36f1e8dcf8b6c6
paul borrego <pborrego@trinity.edu>
2024-11-15 01:09:07 -0600

    shrinking

--- Game.hs
+++ Game.hs
@@ -5,15 +5,20 @@ import Data.Maybe
 type Position = Int
 type Stones = Int
 data Player = PlayerOne | PlayerTwo deriving (Show, Eq)
-type Row = (Int, [Int]) -- should be changed to "type Row = ([Pit], Store)" where Divet has been removed
+type Row = (Int, [Int])
 type Board  = (Row, Row)
-
 type Game = (Player, Board)
+
+--setup
+-----------------------------------------------------
 
 makeGame :: Int -> Game
 makeGame k =
     let pits = [4 | _ <- [1..k]]
     in (PlayerOne,((0, pits),(0, pits)))
+
+------------------------------------------------------
+--move
 
 move :: Game -> Position -> Game
 move game@(p,board) pit =
@@ -22,14 +27,16 @@ move game@(p,board) pit =
     else error "Invalid pit selected"
 
 pickUp :: Game -> Position -> Board
-pickUp (PlayerOne, ((s,one), two)) pit =
-    let (begin,(_:end)) = splitAt pit one
+pickUp (p, ((s1,one), (s2,two))) pit =
+    let (begin,(_:end)) = if p == PlayerOne then splitAt pit one else splitAt pit two
         newRow = begin ++ [0] ++ end
-    in ((s,newRow),two)
-pickUp (PlayerTwo, (one, (s,two))) pit =
-    let (begin,(_:end)) = splitAt pit two
-        newRow = begin ++ [0] ++ end
-    in (one,(s,newRow))
+    in if p == PlayerOne then ((s1,newRow),(s2,two)) else ((s1,one),(s2,newRow))
+
+getStones :: Game -> Position -> Maybe Int
+getStones (player, (one,two)) pit =
+    let (_,side) = sideOf player (one,two)
+        val  = safeBangBang (zip side [0..]) pit
+    in if val == 0 then Nothing else Just val
 
 movePieces :: (Game,Stones) -> Player -> Position -> Game
 movePieces (game@(playersTurn,(one,two)), stones) playersSide pos
@@ -57,49 +64,17 @@ getIncorrect (PlayerTwo,((store,pits),two)) stones pos =
         newBoard = ((store,newPits),two)
     in ((PlayerTwo,newBoard), remain)
 
-
-getStones :: Game -> Position -> Maybe Int
-getStones (player, (one,two)) pit =
-    let (_,side) = sideOf player (one,two)
-        val  = safeBangBang (zip side [0..]) pit
-    in if val == 0 then Nothing else Just val
-
-safeBangBang :: [(Int,Int)] -> Int -> Int
-safeBangBang [] _ = 0
-safeBangBang ((val,pos):xs) pit = if pos == pit then val else safeBangBang xs pit
-
-otherSide :: Player -> Board -> Row
-otherSide PlayerOne (_,board) = board
-otherSide PlayerTwo (board,_) = board
-
-sideOf :: Player -> Board -> Row
-sideOf PlayerOne (board,_) = board
-sideOf PlayerTwo (_,board) = board
-
---Useful when we know that all pits will get a piece
-sowAll :: [Stones] -> [Stones]
-sowAll s = map (\a -> a + 1) s
-
-sowSome :: [Stones] -> Int -> [Stones]
-sowSome pits num = [if y <= num then x + 1 else x | (x,y) <- (zip pits [1..])]
-
 --returns new row and remaining pieces
 sowStones :: [Stones] -> Position -> Stones -> ([Stones],Int)
-sowStones pits 0 stones =
-    let remaining      = stones - (length pits)
-        val            = if remaining >= 0 then sowAll pits else sowSome pits stones
-    in (val, remaining)
 sowStones pits pos stones =
     let (before,after) = splitAt pos pits
         remaining      = stones - (length after)
-        val            = if remaining >= 0 then sowAll after else sowSome after stones
+        val            = if remaining >= 0 then map (\a -> a + 1) after else [if y <= stones then x + 1 else x | (x,y) <- (zip pits [1..])]
         in (before ++ val, remaining)
 
-otherPlayer :: Player -> Player
-otherPlayer PlayerOne = PlayerTwo
-otherPlayer PlayerTwo = PlayerOne
 
--- story two
+-----------------------------------------------------------------------------------------
+-- story two Game Status
 data GameState = Ongoing | Win Player | Tie deriving Show
 
 hasGameEnded :: Game -> Bool
@@ -114,3 +89,20 @@ currGameState state@(_, ((s1,_), (s2,_)))
     | s1 > s2                       = Win PlayerOne
     | s2 > s1                       = Win PlayerTwo
     | otherwise                     = Tie
+
+
+
+-------------------------------------------------------------------------------------------
+-- universal helper Functions
+
+safeBangBang :: [(Int,Int)] -> Int -> Int
+safeBangBang [] _ = 0
+safeBangBang ((val,pos):xs) pit = if pos == pit then val else safeBangBang xs pit
+
+sideOf :: Player -> Board -> Row
+sideOf PlayerOne (board,_) = board
+sideOf PlayerTwo (_,board) = board
+
+otherPlayer :: Player -> Player
+otherPlayer PlayerOne = PlayerTwo
+otherPlayer PlayerTwo = PlayerOne
