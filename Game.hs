190d51b2054e6a3f331761f3e8b2a3fae523dce6
paul borrego <pborrego@trinity.edu>
2024-11-17 17:59:26 -0600

    move

--- Game.hs
+++ Game.hs
@@ -23,8 +23,9 @@ makeGame k =
 move :: Game -> Position -> Game
 move game@(p,board) pit =
     let stones = getStones game pit
-    in if isJust stones then movePieces ((p, pickUp game pit), fromJust stones) p (pit + 1)
-    else error "Invalid pit selected"
+    in case stones of
+        Nothing -> error "Invalid Pit Selected"
+        Just stones -> movePieces ((p, pickUp game pit), stones) p (pit + 1)
 
 pickUp :: Game -> Position -> Board
 pickUp (p, ((s1,one), (s2,two))) pit =
@@ -32,26 +33,21 @@ pickUp (p, ((s1,one), (s2,two))) pit =
         newRow = begin ++ [0] ++ end
     in if p == PlayerOne then ((s1,newRow),(s2,two)) else ((s1,one),(s2,newRow))
 
-getStones :: Game -> Position -> Maybe Int
-getStones (player, (one,two)) pit =
-    let (_,side) = sideOf player (one,two)
-        val  = safeBangBang (zip side [0..]) pit
-    in if val == 0 then Nothing else Just val
 
 movePieces :: (Game,Stones) -> Player -> Position -> Game
 movePieces (game@(playersTurn,(one,two)), stones) playersSide pos
-    | stones <= 0 = (otherPlayer playersTurn, (one, two))
+    | stones <= 0 = checkLanding (game,stones) playersSide (length one) --(otherPlayer playersTurn, (one, two))
     | playersTurn == playersSide = movePieces (getCorrect game stones pos) (otherPlayer playersSide) 0
     | otherwise = movePieces (getIncorrect game stones pos) (otherPlayer playersSide) 0
 
 getCorrect :: Game -> Stones -> Position -> (Game,Int)
 getCorrect (PlayerOne,((store,pits),two)) stones pos =
     let (newPits,remain)     = sowStones pits pos stones
-        (left, newStore)     = if remain > 0 then (remain - 1, store + 1) else (remain,store)
+        (left, newStore)     = if remain > 0 then (remain - 1, store + 1) else (remain - 1,store)
     in ((PlayerOne,((newStore,newPits),two)),left)
 getCorrect (PlayerTwo,(one,(store,pits))) stones pos =
     let (newPits,remain)     = sowStones pits pos stones
-        (left, newStore)     = if remain > 0 then (remain - 1, store + 1) else (remain,store)
+        (left, newStore)     = if remain > 0 then (remain - 1, store + 1) else (remain - 1,store)
     in ((PlayerTwo,(one,(newStore, newPits))), left)
 
 getIncorrect :: Game -> Stones -> Position -> (Game, Int)
@@ -69,9 +65,34 @@ sowStones :: [Stones] -> Position -> Stones -> ([Stones],Int)
 sowStones pits pos stones =
     let (before,after) = splitAt pos pits
         remaining      = stones - (length after)
-        val            = if remaining >= 0 then map (\a -> a + 1) after else [if y <= stones then x + 1 else x | (x,y) <- (zip pits [1..])]
+        val            = if remaining >= 0 then map (\a -> a + 1) after else [if y <= stones then x + 1 else x | (x,y) <- (zip after [1..])]
         in (before ++ val, remaining)
 
+checkLanding :: (Game,Stones) -> Player -> Int -> Game
+checkLanding ((playerTurn,b),0) playerSide len --if ending in your own store
+    | playerSide /= playerTurn = traceShow "73" $ (playerTurn,b)
+    | otherwise = traceShow "74" $ (playerSide,b)
+checkLanding ((playerTurn,b),stones) playerSide len
+    | playerSide /= playerTurn = traceShow "76" $ (playerSide,b)
+    | otherwise = --check where piece landed
+        let position = len + stones
+            check    = getStones (playerSide,b) position
+            newTurn  = otherPlayer playerTurn
+            other    = getStones (newTurn,b) (len + 1 - position)
+        in case check of
+            Nothing -> traceShow "83" $ (newTurn, b)
+            Just 1  -> case other of
+                Nothing -> traceShow "85" $ (newTurn, b)
+                Just y -> traceShow "86" $ (newTurn, addToStore playerTurn b y position)
+            Just x  -> traceShow "87" $ (newTurn, b)
+
+addToStore :: Player -> Board -> Stones -> Position -> Board
+addToStore PlayerOne ((s1,one),two) val pos =
+    let (before,_:after) = splitAt pos one
+    in ((s1 + val + 1,before ++ [0] ++ after),two)
+addToStore PlayerTwo (one,(s2,two)) val pos =
+    let (before,_:after) = splitAt pos two
+    in (one,(s2 + val + 1,before ++ [0] ++ after))
 
 -----------------------------------------------------------------------------------------
 -- story two Game Status
@@ -90,8 +111,6 @@ currGameState state@(_, ((s1,_), (s2,_)))
     | s2 > s1                       = Win PlayerTwo
     | otherwise                     = Tie
 
-
-
 -------------------------------------------------------------------------------------------
 -- universal helper Functions
 
@@ -106,3 +125,10 @@ sideOf PlayerTwo (_,board) = board
 otherPlayer :: Player -> Player
 otherPlayer PlayerOne = PlayerTwo
 otherPlayer PlayerTwo = PlayerOne
+
+getStones :: Game -> Position -> Maybe Int
+getStones (player, (one,two)) pit =
+    let (_,side) = sideOf player (one,two)
+        val  = safeBangBang (zip side [0..]) pit
+    in if val == 0 then Nothing else Just val
+
