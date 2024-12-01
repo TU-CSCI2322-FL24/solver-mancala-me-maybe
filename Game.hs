a025d3d6ae4b7a04a8da6dc127c7c647daec5215
ShadowPikmin <jdcarter432@gmail.com>
2024-12-01 14:10:44 -0600

    Changes implementaton of whoWillWin and bestMove, such that whoWillWin informs player the best outcome for a player given a gamestate and bestMove inform player of the first move that can lead the best possible outcome

--- Game.hs
+++ Game.hs
@@ -12,6 +12,10 @@ data Player = PlayerOne | PlayerTwo deriving (Show, Eq)
 type Row = (Int, [Int])
 type Board  = (Row, Row)
 type Game = (Player, Board)
+type Move = (Player, Position)
+
+data Winner = Win Player | Tie deriving (Show,Eq)
+data GameState = Ongoing | Winner Winner deriving Show
 
 --setup
 -----------------------------------------------------
@@ -22,10 +26,10 @@ makeGame k =
     let pits = [4 | _ <- [1..k]]
     in (PlayerOne,((0, pits),(0, pits)))
 
-move :: Game -> Position -> Maybe Game 
+move :: Game -> Position -> Maybe Game
 move game@(p,board) pit = do
-                          stones <- getStones game pit 
-                          Just (movePieces ((p, pickUp game pit), stones) p (pit + 1)) 
+                          stones <- getStones game pit
+                          Just (movePieces ((p, pickUp game pit), stones) p (pit + 1))
 {-
     let stones = getStones game pit
     in case stones of
@@ -43,13 +47,10 @@ pickUp (p, ((s1,one), (s2,two))) pit =
 
 
 movePieces :: (Game,Stones) -> Player -> Position -> Game
-movePieces (game@(playersTurn,(one,two)), stones) playersSide pos
+movePieces (game@(playersTurn,((_,one),_)), stones) playersSide pos
     | stones <= 0                = checkLanding (game,stones) playersSide (length one) --(otherPlayer playersTurn, (one, two))
     | playersTurn == playersSide = movePieces (getCorrect game stones pos) (otherPlayer playersSide) 0
     | otherwise                  = movePieces (getIncorrect game stones pos) (otherPlayer playersSide) 0
-    -- | playersTurn == playersSide = movePieces (getCorrect game stones pos) (otherPlayer playersSide) 0
-    -- | otherwise = movePieces (getIncorrect game stones pos) (otherPlayer playersSide) 0
-    -- | stones <= 0 = traceShow pos $ if emptyPit (playersTurn, (one,two)) pos then takeStoneFromOther (playersTurn, (one,two)) pos else (otherPlayer playersTurn, (one, two))
 
 getCorrect :: Game -> Stones -> Position -> (Game,Int)
 getCorrect (PlayerOne,((store,pits),two)) stones pos =
@@ -81,47 +82,32 @@ sowStones pits pos stones =
 
 checkLanding :: (Game,Stones) -> Player -> Int -> Game
 checkLanding ((playerTurn,b),0) playerSide len --if ending in your own store
-    | playerSide /= playerTurn = (playerTurn,b)
-    | otherwise = (playerSide,b)
+    | playerSide /= playerTurn = (playerTurn,b) --you ended in your own store
+    | otherwise = (otherPlayer playerSide,b)
 checkLanding ((playerTurn,b),stones) playerSide len
-    | playerSide /= playerTurn = (playerSide,b)
-    | otherwise = --check where piece landed
+    | playerSide == playerTurn = (otherPlayer playerTurn,b) --you ended on the other side
+    | otherwise = --you ended on your side
         let position = len + stones
-            check    = getStones (playerSide,b) position
-            newTurn  = otherPlayer playerTurn
-            other    = getStones (newTurn,b) (len + 1 - position)
+            check    = getStones (playerTurn,b) position
+            otherPosition = len - 1 - position
+            other    = getStones (playerSide,b) otherPosition
         in case check of
-            Nothing -> (newTurn, b)
+            Nothing -> (playerSide, b)
             Just 1  -> case other of
-                Nothing -> (newTurn, b)
-                Just y -> (newTurn, addToStore playerTurn b y position)
-            Just x  -> (newTurn, b)
+                Nothing -> (playerSide, b)
+                Just y -> (playerSide, addToStore playerTurn b y position otherPosition)
+            Just x  -> (playerSide, b)
 
-addToStore :: Player -> Board -> Stones -> Position -> Board
-addToStore PlayerOne ((s1,one),two) val pos =
-    let (before,_:after) = splitAt pos one
-    in ((s1 + val + 1,before ++ [0] ++ after),two)
-addToStore PlayerTwo (one,(s2,two)) val pos =
-    let (before,_:after) = splitAt pos two
-    in (one,(s2 + val + 1,before ++ [0] ++ after))
+addToStore :: Player -> Board -> Stones -> Position -> Position -> Board
+addToStore PlayerOne ((s1,one),(s2,two)) val pos1 pos2 =
+    let (before,_:after) = splitAt pos1 one
+        (oB,_:oA)        = splitAt pos2 two
+    in ((s1 + val + 1,before ++ [0] ++ after),(s2,(oB ++ [0] ++ oA)))
+addToStore PlayerTwo ((s1,one),(s2,two)) val pos1 pos2 =
+    let (before,_:after) = splitAt pos1 two
+        (oB,_:oA)        = splitAt pos2 one
+    in ((s1, oB ++ [0] ++ oA),(s2 + val + 1,before ++ [0] ++ after))
 
--- emptyPit :: Game -> Position -> Bool
--- emptyPit game@(pl, (one, two)) pit = 
-                                   -- let Just stone = getStones (pl, (one,two)) pit
-                                   -- in traceShow stone $ stone == 1
--- 
--- takeStoneFromOther :: Game -> Position -> Game 
--- takeStoneFromOther game@(PlayerOne, (one,two)) pos = 
-                                             -- let curStore = (fst one) 
-                                                 -- aux oldX oldY ((st1, []), (st2, [])) count = (PlayerTwo, ((st1, oldX), (st2, oldY))) 
-                                                 -- aux oldX oldY ((st1, (x:xs)), (st2, (y:ys))) count = if count == pos then (PlayerTwo, (((st1 + x + y), oldX ++ (0:xs)), (st2, oldY ++ (0:ys)))) else aux (x:oldX) (0:oldY) ((st1, xs), (st2, ys)) (count + 1) 
-                                            -- in aux [] [] (one, two) 0   
--- 
--- takeStoneFromOther game@(PlayerTwo, (one,two)) pos = 
-                                             -- let curStore = (fst one) 
-                                                 -- aux oldX oldY ((st1, []), (st2, [])) count = (PlayerOne, ((st1, oldX), (st2, oldY))) 
-                                                 -- aux oldX oldY ((st1, (x:xs)), (st2, (y:ys))) count = if count == pos then (PlayerTwo, ((st1, oldX ++ (0:xs)), ((st2 + x + y), oldY ++ (y:ys)))) else aux (x:oldX) (0:oldY) ((st1, xs), (st2, ys)) (count + 1) 
-                                            -- in aux [] [] (one, two) 0   
 ------------------------------------------------------------------------------------------
 --Story 4: possibleMoves
 
@@ -131,8 +117,8 @@ possibleMoves game@(_,((_,one),_)) =
     in catMaybes [move game pos | pos <- [0..len]]
 
 ------------------------------------------------------------------------------------------
-
 -- Story 5: Pretty-print a game into a string as a Mancala board
+
 prettyPrintGame :: Game -> String
 prettyPrintGame (player, ((storeOne, pitsOne), (storeTwo, pitsTwo))) =
     let boardWidth = length pitsOne
@@ -145,13 +131,13 @@ prettyPrintGame (player, ((storeOne, pitsOne), (storeTwo, pitsTwo))) =
 ------------------------------------------------------------------------------------------
 -- Story 8: Game State and Winner
 
-data Winner = Win Player | Tie deriving Show
-data GameState = Ongoing | Winner Winner deriving Show
 
 hasGameEnded :: Game -> Bool
-hasGameEnded (_, ((_,one), (_,two)))
+hasGameEnded (_, ((storeOne,one), (storeTwo,two)))
     | all (== 0) one    = True
     | all (== 0) two    = True
+    | storeOne > ((length one) * 8) = True
+    | storeTwo > ((length two) * 8) = True 
     | otherwise         = False
 
 whoWon :: Game -> Maybe Winner
@@ -167,38 +153,75 @@ currGameState game =
 
 ------------------------------------------------------------------------------------------
 -- Story 9: Guess Moves
+whoWillWin :: Game -> Player -> Winner
+-- depth first search 
+whoWillWin game pl = 
+   let aux game = traceShow ((show (hasGameEnded game)) ++ " : " ++ (show game)) $ if (hasGameEnded game) then fromJust (whoWon game) else compareListOutcome pl [aux g | g <- (possibleMoves game)]   
+   in aux game 
 
-whoWillWin :: Game -> (Game,GameState)
-whoWillWin game@(player, _) =
-    let moves          = possibleMoves game
-        (fst:outcomes) = [(g,currGameState g) | g <- moves]
-    in helpWho outcomes fst player
+-- breadth first search
+{-- whoWillWin game pl = findOutcome (possibleMoves game) pl (fromMaybe (Win (otherPlayer pl)) (whoWon game)) 
+ 
+findOutcome :: [Game] -> Player -> Winner -> Winner
+findOutcome [] pl curOutcome = curOutcome 
+findOutcome games pl curOutcome = 
+   let newOutcome = bestOutcome [fromJust (whoWon g) | g <- games, hasGameEnded g] pl
+       aux [] = [] 
+       aux (g:gs) = if not (hasGameEnded g) then (possibleMoves g) ++ (aux gs) else aux gs 
+       outcome = compareOutcome pl (fromMaybe (Win (otherPlayer pl)) newOutcome) curOutcome 
+   in traceShow (length games) $ if outcome == (Win pl) then outcome else findOutcome (aux games) pl outcome  
 
-helpWho :: [(Game,GameState)] -> (Game, GameState) -> Player -> (Game, GameState)
-helpWho [] game _ = game
-helpWho ((g, Tie):xs) (game, gameState) player = helpWho xs (g, Tie) player
-helpWho ((g, Win winner):xs) game player
-    | winner == player = (g, Win player)
-    | otherwise        = helpWho xs game player
+bestOutcome :: [Winner] -> Player -> Maybe Winner
+bestOutcome [] pl = Nothing    
+bestOutcome (w:ws) pl = 
+    let aux [] best = Just best
+        aux (w:ws) best = traceShow (("Game Result: ") ++ (show w)) $ if best == (Win pl) then Just best else if ((w == (Win pl)) || ((w == Tie) && (best == (Win (otherPlayer pl))))) then aux ws w else aux ws best
+    in aux ws w 
+--}
+
+-- original Stragety
+{-- whoWillWin :: Game -> Winner
+whoWillWin game@(player, _) = ongoingToWinner (possibleMoves game) player
+
+
+ongoingToWinner :: [Game] -> Player -> Winner
+ongoingToWinner games player =
+    let moves         = traceShow (show games) $ allMoves games
+        outcomes      = [(g,currGameState g) | g <- moves]
+        (ongoing,bestState) = foldr sortGameState ([],Win (otherPlayer player)) outcomes
+    in if bestState == Win player then Win player
+       else if length ongoing == 0 then bestState
+            else ongoingToWinner ongoing (otherPlayer player)
+
+sortGameState :: (Game,GameState) -> ([Game],Winner) -> ([Game], Winner)
+sortGameState (g,Ongoing) (a,b) = ((g:a), b)
+sortGameState ((player,_), Winner Tie) (a,b) = if b == Win player then (a,b) else (a, Tie)
+sortGameState ((player,_), Winner (Win p)) (a,b) = if player == p then (a, Win p) else (a,b)
+
+allMoves :: [Game] -> [Game]
+allMoves [] = []
+allMove (g:gs) = traceShow ((show g) ++ " and " ++ (show gs)) $ (possibleMoves g) ++ (allMoves gs)
+--}
 -------------------------------------------------------------------------------------------
-
 -- Story 11 & 12: Text Format & readGame (read text format)
 
 readGame :: String -> Game
 readGame input =
     let linesInput = lines input
-    in if length linesInput /= 4
-        then error "Invalid input: Expected exactly 4 lines."
-        else
-            let [line1, line2, line3, line4] = linesInput
-                -- Parse pitsTwo and pitsOne
-                pitsTwo = case traverse readMaybe (words line1) of
+    in case linesInput of
+        [] -> error "Invalid input: 0 lines provided, expected 4."
+        [_] -> error "Invalid input: 1 line provided, expected 4."
+        [_, _] -> error "Invalid input: 2 lines provided, expected 4."
+        [_, _, _] -> error "Invalid input: 3 lines provided, expected 4."
+        [line1, line2, line3, line4] -> 
+            -- parse pitsTwo and pitsOne
+            let pitsTwo = case traverse readMaybe (words line1) of
                     Just pits -> reverse pits
-                    Nothing -> error "Invalid input: Pits (line 1) must be integers."
+                    Nothing -> error "Invalid input: P2 Pits (line 1) must be integers."
                 pitsOne = case traverse readMaybe (words line3) of
                     Just pits -> pits
-                    Nothing -> error "Invalid input: Pits (line 3) must be integers."
-                -- Parse line2 for P1 and P2 store values
+                    Nothing -> error "Invalid input: P1 Pits (line 3) must be integers."
+                -- parse line2 for P1 and P2 store values
                 wordsLine2 = words line2
                 _ = if length wordsLine2 /= 4 || head wordsLine2 /= "P1:" || wordsLine2 !! 2 /= "P2:"
                         then error "Invalid input: Line 2 must follow the format 'P1: <int> P2: <int>'."
@@ -209,7 +232,7 @@ readGame input =
                 p2Store = case readMaybe (wordsLine2 !! 3) of
                     Just s -> s
                     Nothing -> error "Invalid input: P2 store value must be an integer."
-                -- Parse line4 for player
+                -- parse line4 for player
                 wordsLine4 = words line4
                 _ = if length wordsLine4 /= 2 || head wordsLine4 /= "Player:"
                         then error "Invalid input: Line 4 must follow the format 'Player: PlayerOne' or 'Player: PlayerTwo'."
@@ -219,6 +242,7 @@ readGame input =
                     "PlayerTwo" -> PlayerTwo
                     _ -> error "Invalid input: Player must be 'PlayerOne' or 'PlayerTwo'."
             in (player, ((p1Store, pitsOne), (p2Store, pitsTwo)))
+        _ -> error "Invalid input: More than 4 lines provided, expected exactly 4."
 
 -------------------------------------------------------------------------------------------
 -- Story 13: showGame (takes a game and puts it into string format)
@@ -239,52 +263,85 @@ showGame (player, ((storeOne, pitsOne), (storeTwo, pitsTwo))) =
 -- Story 10: Best Move 
 
 bestMove :: Game -> Game 
-bestMove game@(pt, (one,two)) = 
-    let moves = possibleMoves (pt, (one,two)) 
-        repeats = stopAtStore moves pt 
-    in if (null repeats) then findBestMove moves (pt, (one,two)) else findBestMove repeats (pt, (one,two)) 
+bestMove game@(pl, (one,two)) = 
+    let (x:xs) = possibleMoves game 
+        aux [] pl (gameState, result) = gameState
+        aux (y:ys) pl (gameState, result) = 
+            let newResult = whoWillWin y pl   
+            in if result == (Win pl) then gameState else if newResult == (Win pl) then y else if (newResult == Tie) && (result == (Win (otherPlayer pl))) then aux ys pl (y, newResult) else aux ys pl (gameState, result) 
+    in aux xs pl (x, (whoWillWin x pl)) 
 
-stopAtStore :: [Game] -> Player -> [Game] 
+-- Keep for Sprint 3 
+{-- bestMove :: Game -> Game
+bestMove game@(pt, (one,two)) =
+    let moves = possibleMoves (pt, (one,two))
+        repeats = stopAtStore moves pt
+    in if (null repeats) then findBestMove moves (pt, (one,two)) else findBestMove repeats (pt, (one,two))
+
+stopAtStore :: [Game] -> Player -> [Game]
 stopAtStore moves pt = [(mPt, state) | (mPt, state) <- moves, mPt == pt]
 
-findBestMove :: [Game] -> Game -> Game 
-findBestMove (move:moves) orgState = 
-    let aux [] (bestM, bestR) = bestM 
-        aux (x:xs) (bestM, bestR) =  
-            let rank = getRank x orgState 
+findBestMove :: [Game] -> Game -> Game
+findBestMove (move:moves) orgState =
+    let aux [] (bestM, bestR) = bestM
+        aux (x:xs) (bestM, bestR) =
+            let rank = getRank x orgState
             in if rank > bestR then aux xs (x, rank) else aux xs (bestM, bestR)
-    in aux moves (move, (getRank move orgState)) 
+    in aux moves (move, (getRank move orgState))
 
-getRank :: Game -> Game -> Int 
-getRank (nPt, (nOne,nTwo)) (PlayerOne, (one,two)) = 
+getRank :: Game -> Game -> Int
+getRank (nPt, (nOne,nTwo)) (PlayerOne, (one,two)) =
     let captureScore = (fst nOne) - (fst one)
         otherSideStone = (- (sum [new - cur | (new, cur) <- (zip (snd nTwo) (snd two))]))
-        opCapPotent = opCap (nPt, (nOne, nTwo)) 
-        allowOpRepeat = if (nPt == PlayerOne) || (not (canOpRepeat (nPt, (nOne,nTwo)))) then 0 else (-3)   
-    in captureScore + otherSideStone + opCapPotent + allowOpRepeat 
+        opCapPotent = opCap (nPt, (nOne, nTwo))
+        allowOpRepeat = if (nPt == PlayerOne) || (not (canOpRepeat (nPt, (nOne,nTwo)))) then 0 else (-3)
+    in captureScore + otherSideStone + opCapPotent + allowOpRepeat
 
-getRank (nPt, (nOne, nTwo)) (PlayerTwo, (one,two)) = 
-    let captureScore = (fst nTwo) - (fst two) 
+getRank (nPt, (nOne, nTwo)) (PlayerTwo, (one,two)) =
+    let captureScore = (fst nTwo) - (fst two)
         otherSideStone = (-(sum [new - cur | (new, cur) <- (zip (snd nOne) (snd one))]))
-        opCapPotent = opCap (nPt, (nOne, nTwo)) 
-        allowOpRepeat = if (nPt == PlayerTwo) || (not (canOpRepeat (nPt, (nOne, nTwo)))) then 0 else (-3) 
-    in captureScore + otherSideStone + opCapPotent + allowOpRepeat 
+        opCapPotent = opCap (nPt, (nOne, nTwo))
+        allowOpRepeat = if (nPt == PlayerTwo) || (not (canOpRepeat (nPt, (nOne, nTwo)))) then 0 else (-3)
+    in captureScore + otherSideStone + opCapPotent + allowOpRepeat
 
 opCap :: Game -> Int
-opCap (PlayerOne, (one,two)) = 
+opCap (PlayerOne, (one,two)) =
     let moves = possibleMoves (PlayerOne, (one,two))
-    in foldr (\ (p, (o,t)) recVal -> max ((fst o) - (fst one)) recVal) 0 moves 
+    in foldr (\ (p, (o,t)) recVal -> max ((fst o) - (fst one)) recVal) 0 moves
 
-opCap (PlayerTwo, (one, two)) = 
+opCap (PlayerTwo, (one, two)) =
     let moves = possibleMoves (PlayerTwo, (one,two))
-    in foldr (\ (p, (o,t)) recVal -> max (((fst t) - (fst two)) - 1) recVal) 0 moves 
+    in foldr (\ (p, (o,t)) recVal -> max (((fst t) - (fst two)) - 1) recVal) 0 moves
 
-canOpRepeat :: Game -> Bool 
-canOpRepeat (pt, (one,two)) = 
-    let moves = possibleMoves (pt, (one, two)) 
-    in foldr (\(p,state) recVal -> (p == pt) || recVal) False moves 
+canOpRepeat :: Game -> Bool
+canOpRepeat (pt, (one,two)) =
+    let moves = possibleMoves (pt, (one, two))
+    in foldr (\(p,state) recVal -> (p == pt) || recVal) False moves
+--} 
 -------------------------------------------------------------------------------------------
--- universal helper Functions
+-- Story 14: IO Actions
+
+writeGame :: Game -> FilePath -> IO ()
+writeGame game filePath = do
+    writeFile filePath (showGame game)
+
+loadGame :: FilePath -> IO Game
+loadGame filePath = do
+    contents <- readFile filePath
+    return (readGame contents)
+
+putBestMove :: Game -> IO ()
+putBestMove game = do
+    putStrLn "Old Game:"
+    putStrLn $ showGame game
+    putStrLn "Best Move:"
+    putStrLn $ showGame (bestMove game)
+
+main :: IO ()
+main = undefined
+
+-------------------------------------------------------------------------------------------
+-- Universal Helper Functions
 
 safeBangBang :: [(Int,Int)] -> Int -> Int
 safeBangBang [] _ = 0
@@ -303,3 +360,12 @@ getStones (player, (one,two)) pit =
     let (_,side) = sideOf player (one,two)
         val  = safeBangBang (zip side [0..]) pit
     in if val == 0 then Nothing else Just val
+
+compareListOutcome :: Player -> [Winner] -> Winner
+compareListOutcome pl (w:ws) = 
+   let aux [] outcome = outcome
+       aux (x:xs) outcome = aux xs (compareOutcome pl x outcome) 
+   in aux ws w 
+compareOutcome :: Player -> Winner -> Winner -> Winner
+compareOutcome pl new current = if new == (Win (otherPlayer pl)) then current else if new == (Win pl) then new else if (new == (Win pl)) && (current == (Win (otherPlayer pl))) then new else current 
+
