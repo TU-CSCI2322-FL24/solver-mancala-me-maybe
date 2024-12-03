b2d1b1ea34f8ec7e6adbe9b6b44c8a39132a7d96
Cody Estes <cestes@trinity.edu>
2024-12-03 13:51:56 -0600

    Removed Main.hs and reconfigured the project to allow for both a main in Game.hs and in Testing.hs.

--- Testing.hs
+++ Testing.hs
@@ -1,12 +1,13 @@
 module Testing where
-import Game 
+
+import Game
 import Data.Semigroup
-import Dictionaries
+--import Dictionaries
 import Control.Monad.Extra
 import Test.Grader.Core
-import Test.Grader.Rubric
+import Test.Grader.Rubric ()
 import Test.Grader.Eval
-import Test.Grader.Tests
+import Test.Grader.Tests ( shouldBeDefined )
 import Test.Grader.Parsing.Util
 import Test.Grader.Parsing
 import Data.List
@@ -16,9 +17,14 @@ testScore :: Grader String
 testScore = assess "score" 3 $ do
     check "that score is defined" $ shouldBeDefined "score"
 
-runTests :: Int -> Bool -> IO ()
-runTests verb force = do
+runOurTests :: Int -> Bool -> IO ()
+runOurTests verb force = do
+    let tree = undefined
     let a = runGrader tree
     format <- makeFormat verb force "projectDesc.yaml"
     runRWST a () format 
     return ()
+
+main :: IO() 
+main = do
+    runOurTests 1 True
